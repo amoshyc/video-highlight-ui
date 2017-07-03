3639c0b0984409506ea841a4bdef9a40589fc47e
amoshyc <amoshuangyc@gmail.com>
2017-07-04 00:16:34 +0800

    Swith server from flask to tornado

--- server.py
+++ server.py
@@ -1,59 +1,84 @@
 import argparse
 import pathlib
-import youtube_dl
-from urllib.parse import urlparse
-from flask import Flask, request, render_template, send_from_directory
+import multiprocessing
+import json
+import sys
 
-app = Flask(__name__)
+import tornado.ioloop
+import tornado.web
+import tornado.escape
 
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+clip_process = None
 video_src = None
 video_type = None
 
 
-@app.route('/')
-def index():
-    res = render_template(
-        'index.html', video_src=video_src, video_type=video_type)
+def clip(starts, ends):
+    video = VideoFileClip(video_src)
+    clips = [video.subclip(s, e) for s, e in zip(starts, ends)]
+    result = concatenate_videoclips(clips)
+    result_path = pathlib.Path(video_src).parent / 'clip.{}'.format(video_type)
+    result.write_videofile(str(result_path), write_logfile=True)
 
-    return res
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            'templates/index.html', video_src=video_src, video_type=video_type)
 
 
-@app.route('/local/<path:path>', methods=['GET'])
-def download_file(path):
-    path = pathlib.Path(path)
-    directory = str(path.parent)
-    filename = str(path.name)
-    return send_from_directory(directory=directory, filename=filename)
+class ClipTriggerHandler(tornado.web.RequestHandler):
+    def post(self):
+        arg = tornado.escape.json_decode(self.request.body)
+        starts = map(float, arg['starts'])
+        ends = map(float, arg['ends'])
+
+        log_file = pathlib.Path(video_src).with_suffix('.txt')
+        with open(str(log_file), 'w') as f:
+            data = ['{} {}'.format(s, e) for s, e in zip(starts, ends)]
+            f.write('\n'.join(data))
+
+        clip_process = multiprocessing.Process(
+            target=clip, args=(starts, ends))
+        clip_process.start()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', help="youtube_url or path/to/video")
+    parser.add_argument('path', help="path/to/video")
     args = parser.parse_args()
 
     global video_src
     global video_type
 
-    if urlparse(args.path).scheme == '':  # arg is path
-        path = pathlib.Path(args.path)
+    path = pathlib.Path(args.path).expanduser().absolute()
 
-        if not path.exists():
-            print('Error: "{}" not exists'.format(path))
-            print('Program exits.')
-            return
-        if path.suffix.lower() not in ('.mp4', '.webm', '.ogg'):
-            print('Error: "{}" is not supported'.format(path.suffix))
-            print('Program exits.')
-            return
+    if not path.exists():
+        print('Error: "{}" not exists'.format(path))
+        print('Program exits.')
+        return
+    if path.suffix.lower() not in ('.mp4', '.webm', '.ogg'):
+        print('Error: "{}" is not supported'.format(path.suffix))
+        print('Program exits.')
+        return
 
-        video_src = str(path)
-        video_type = path.suffix[1:]
-    else:  # arg is url
-        video_src = args.path
-        video_type = 'youtube'
+    # prevent path start from root to cause url has double slash
+    video_src = str(path)
+    video_type = path.suffix[1:]
 
-    app.run()
+    app = tornado.web.Application(
+        [(r'/', IndexHandler),
+         (r'/static/(.*)', tornado.web.StaticFileHandler, {
+             'path': './static'
+         }), (r'/local/@(.*)', tornado.web.StaticFileHandler, {
+             'path': str(path.parent)
+         }), (r'/clip_trigger', ClipTriggerHandler)])
+
+    app.listen(8787)
+    tornado.ioloop.IOLoop.current().start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- static/script.js
+++ static/script.js
@@ -44,16 +44,34 @@ function remove_row(id) {
     $('#row' + id).remove();
 }
 
+function get_labels() {
+    var rows = $('#labels-table tbody tr').get();
+    var starts = [];
+    var ends = [];
+    var ids = [];
+    if (rows.length == 0) { // no labels
+        return;
+    }
+
+    $.each(rows, function (idx, row) {
+        starts.push(parseFloat($(row).children('td').eq(0).text()));
+        ends.push(parseFloat($(row).children('td').eq(1).text()));
+        ids.push($(row).attr('id'));
+    });
+
+    return [starts, ends, ids]
+}
+
 function sort_labels_by_column(idx) { // idx=0: start; idx=1: end
-    var data = $('#labels-table tbody tr').get();
-    data.sort(function (a, b) {
+    var rows = $('#labels-table tbody tr').get();
+    rows.sort(function (a, b) {
         var value_a = parseFloat($(a).children('td').eq(idx).text());
         var value_b = parseFloat($(b).children('td').eq(idx).text());
         if (value_a < value_b) return -1;
         if (value_a > value_b) return +1;
         return 0;
     });
-    $.each(data, function (index, row) {
+    $.each(rows, function (index, row) {
         $('#labels-table tbody').append(row);
     });
 }
@@ -72,20 +90,10 @@ function video_toggle_pause() {
 }
 
 function video_preview() {
-    // get times
-    var data = $('#labels-table tbody tr').get();
-    var starts = [];
-    var ends = [];
-    var ids = [];
-    if (data.length == 0) { // no labels
-        return;
-    }
-
-    $.each(data, function (idx, row) {
-        starts.push(parseFloat($(row).children('td').eq(0).text()));
-        ends.push(parseFloat($(row).children('td').eq(1).text()));
-        ids.push($(row).attr('id'));
-    });
+    var labels = get_labels();
+    var starts = labels[0];
+    var ends = labels[1];
+    var ids = labels[2];
 
     // play each segment by changing player time to start and detect end by time_update
     var idx = 0;
@@ -116,6 +124,18 @@ function video_preview() {
 
     // register time update
     player.on('timeupdate', time_update);
+}
+
+function clip_trigger() {
+    var labels = get_labels();
+    var data = {
+        'starts': labels[0],
+        'ends': labels[1]
+    }
+
+    $.post('/clip_trigger', JSON.stringify(data), function() {
+        console.log('start clipping');
+    });
 }
 
 function init_keymap() {
