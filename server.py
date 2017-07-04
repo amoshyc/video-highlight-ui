9d522b34e0a1b01e435baa34c44ef201ed9260d7
amoshyc <amoshuangyc@gmail.com>
2017-07-04 11:58:22 +0800

    Fix bugs

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import argparse
 import pathlib
-import multiprocessing
 import json
 import sys
+import multiprocessing as mp
 
 import tornado.ioloop
 import tornado.web
@@ -20,28 +20,26 @@ def clip(starts, ends):
     clips = [video.subclip(s, e) for s, e in zip(starts, ends)]
     result = concatenate_videoclips(clips)
     result_path = pathlib.Path(video_src).parent / 'clip.{}'.format(video_type)
-    result.write_videofile(str(result_path), write_logfile=True)
+    result.write_videofile(str(result_path), threads=3, write_logfile=True)
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render(
-            'templates/index.html', video_src=video_src, video_type=video_type)
+        self.render('index.html', video_src=video_src, video_type=video_type)
 
 
 class ClipTriggerHandler(tornado.web.RequestHandler):
     def post(self):
         arg = tornado.escape.json_decode(self.request.body)
-        starts = map(float, arg['starts'])
-        ends = map(float, arg['ends'])
+        starts = list(map(float, arg['starts']))
+        ends = list(map(float, arg['ends']))
 
         log_file = pathlib.Path(video_src).with_suffix('.txt')
         with open(str(log_file), 'w') as f:
             data = ['{} {}'.format(s, e) for s, e in zip(starts, ends)]
             f.write('\n'.join(data))
 
-        clip_process = multiprocessing.Process(
-            target=clip, args=(starts, ends))
+        clip_process = mp.Process(target=clip, args=(starts, ends))
         clip_process.start()
 
 
@@ -64,17 +62,15 @@ def main():
         print('Program exits.')
         return
 
-    # prevent path start from root to cause url has double slash
     video_src = str(path)
     video_type = path.suffix[1:]
 
-    app = tornado.web.Application(
-        [(r'/', IndexHandler),
-         (r'/static/(.*)', tornado.web.StaticFileHandler, {
-             'path': './static'
-         }), (r'/local/@(.*)', tornado.web.StaticFileHandler, {
-             'path': str(path.parent)
-         }), (r'/clip_trigger', ClipTriggerHandler)])
+    app = tornado.web.Application([
+        (r'/', IndexHandler),
+        (r'/clip_trigger', ClipTriggerHandler),
+        (r'/static/(.*)', tornado.web.StaticFileHandler, { 'path': './static' }),
+        (r'/local/@(.*)', tornado.web.StaticFileHandler, { 'path': str(path.parent) })
+    ]) # yapf: disable
 
     app.listen(8787)
     tornado.ioloop.IOLoop.current().start()
